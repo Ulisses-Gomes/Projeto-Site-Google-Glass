cc53292ffca93e65b2470e2041a08592a83b1ca4
Ulisses Gomes <ulissesgomes2005@gmail.com>
2021-05-09 22:26:02 -0300

    Criando a function calcTot no JS
    Função que calcula o valor do produto no formulário da pagina fale-conosco.

--- _javascript/funcoes.js
+++ _javascript/funcoes.js
@@ -2,3 +2,9 @@
 function mudaFoto(foto){
     document.getElementById("icone").src  = foto;
 }
+
+function calTotal(){
+    const qtd = parseInt(document.getElementById('cQtd').value)
+    const total = qtd * 1500;
+    document.getElementById('cTot').value = total
+}
